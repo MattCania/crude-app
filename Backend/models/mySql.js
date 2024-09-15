4122fc2ac7383d248e60f811e8794557f0eb5c49
Matthew <matthewgab24@gmail.com>
2024-09-15 12:58:06 +0800

    first commit

---
+++ Backend/models/mySql.js
@@ -0,0 +1,29 @@
+
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'crude_db',
+	connectionLimit: 10
+})
+
+const selectValues = (data, callback) => {
+	const selectQry = 'SELECT * FROM bsit2b';
+
+	pool.query(selectQry, (err, result) => {
+		if (err) {
+			console.log('Query Error', err);
+			return callback(err);
+		}
+		callback(null, result);
+	})
+
+
+
+}
+
+exports = {
+	selectValues
+}
